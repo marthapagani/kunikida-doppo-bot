f30712cce51d9e13e2c74f993842bd94e2e40893
Martha Irena <paganimartha@live.com>
2019-08-19 21:03:01 -0300

    Fixed code error

--- bot.py
+++ bot.py
@@ -13,11 +13,9 @@ def randomPic(path):
         pic = path + i
     return pic
     
-def tweetPicTrue(pic):
+def tweetPic(pic):
     photo = open(pic, 'rb')
     tweet = config.twitter.upload_media(media=photo)
     config.twitter.update_status(status='', media_ids=[tweet['media_id']])
 
-while True:
-    print(randomPic(path))
-    sleep(hours)
+tweetPic(randomPic(path))
