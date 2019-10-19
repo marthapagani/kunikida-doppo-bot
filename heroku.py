39f5b2ff014b4a9c1d840d0baea103b68acaab98
Martha Irena <cibertetica@hotmail.com>
2019-10-19 08:35:44 -0300

    New images

--- heroku.py
+++ heroku.py
@@ -1,19 +1,31 @@
 from time import sleep
-from os import listdir, environ
 from twython import Twython
 
-locale = 'images/'
+# API KEYS
+APP_KEY = 'QvZ9OvpEA6yxgd8ediZCvUhiw'
+APP_SECRET = 'E6urktic3goTW5rajerHhxCT7DrEnwbpGFIaET0zkIcLdbJUJt'
+# ACCESS TOKEN
+OAUTH_TOKEN = '1148803609422905344-R4ICLi9gpWVimg40oAzVuJjSc28mXC'
+OAUTH_TOKEN_SECRET = 'Z8GzqSFit01M67jmiw2dLRVQoR2iAf6VLVo1pfeH2lxf7'
 
-CONSUMER_KEY = environ['CONSUMER_KEY']
-CONSUMER_SECRET = environ['CONSUMER_SECRET']
-ACCESS_KEY = environ['ACCESS_KEY']
-ACCESS_SECRET = environ['ACCESS_SECRET']
+twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
-twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
+listing = [
+    'https://raw.githubusercontent.com/cibertetica/kunikida-doppo-bot/master/images/1.png',
+    'https://raw.githubusercontent.com/cibertetica/kunikida-doppo-bot/master/images/2.png',
+    'https://raw.githubusercontent.com/cibertetica/kunikida-doppo-bot/master/images/3.png',
+    'https://raw.githubusercontent.com/cibertetica/kunikida-doppo-bot/master/images/4.png',
+    'https://raw.githubusercontent.com/cibertetica/kunikida-doppo-bot/master/images/5.png',
+    'https://raw.githubusercontent.com/cibertetica/kunikida-doppo-bot/master/images/6.png',
+    'https://raw.githubusercontent.com/cibertetica/kunikida-doppo-bot/master/images/7.png',
+    'https://raw.githubusercontent.com/cibertetica/kunikida-doppo-bot/master/images/8.png',
+    'https://raw.githubusercontent.com/cibertetica/kunikida-doppo-bot/master/images/9.png',
+    'https://raw.githubusercontent.com/cibertetica/kunikida-doppo-bot/master/images/10.png',
+]
 
-for i in listdir(locale):
-    pic = locale + i
-    photo = open(pic, 'rb')
+for i in listing:
+    '''photo = open(i, 'rb')
     tweet = twitter.upload_media(media=photo)
     twitter.update_status(status='', media_ids=[tweet['media_id']])
-    sleep(18000)
+    sleep(18000)'''
+    print(i)
